711f1ca31df77355579147331bb63493778e3220
joachim-gassen <gassen@wiwi.hu-berlin.de>
2018-01-06 13:40:08 +0100

    Added prepare_quantile_trend_graph()

---
+++ R/prepare_quantile_trend_graph.R
@@ -0,0 +1,42 @@
+#' @title Prepares a Quantile Trend Graph
+#'
+#' @description
+#' Reads a data frame containing two variables (an ordered factor and a numeric variable)
+#' and plots the quantiles of the numeric variables
+#' by the ordered factor.
+#'
+#' @param df Data frame containing the ordered factor and the numerical variable
+#' to be plotted
+#' @param xvar a string countaining column name of the ordered factor
+#' @param quantiles a numerical vector containing the quantiles that are to be plotted
+#'
+#' @return A list containing two items:
+#' \itemize{
+#'  \item{"df"}{A data frame containg the plotted means and standard errors}
+#'  \item{"plot}{The plot as returned by ggplot}
+#' }
+#'
+#' @examples
+#' df <- data.frame(year = floor(time(EuStockMarkets)), DAX = EuStockMarkets[,"DAX"])
+#' graph <- prepare_quantile_trend_graph(df, "year", c(0.05, 0.25, 0.5, 0.75, 0.95))
+#' graph$plot
+#' @export
+
+prepare_quantile_trend_graph <- function(df, xvar, quantiles = c(0.05, 0.25, 0.5, 0.75, 0.95)) {
+  # ___BUG__ Not sure how this will work if ts_id factors cannot be coerced to numeric
+  df[,xvar] <- as.numeric(gsub("[^0-9\\.]", "", as.character(df[,xvar])))
+  q <- sapply(quantiles,
+              function(x){by(df[,2], df[, xvar], stats::quantile, na.rm=TRUE,x)})
+  q <- data.frame(as.numeric(rownames(q)),q)
+  colnames(q)[1] <- xvar
+  colnames(q)[2:(length(quantiles) + 1)] <- paste0(rep("q", length(quantiles)), quantiles*100)
+  gg <- tidyr::gather_(data = q,
+                       key_col = "quantile",
+                       value_col = colnames(df)[!(xvar == colnames(df))],
+                       gather_cols = colnames(q)[!(xvar == colnames(q))])
+  plot <- ggplot2::ggplot(gg) + ggplot2::ylab(colnames(df[ncol(df)])) +
+    ggplot2::geom_line(ggplot2::aes_string(x=xvar, y=gg[,3], color="quantile"))+
+    ggplot2::scale_color_discrete(labels=quantiles)
+  list(df = gg, plot = plot)
+}
+
---
+++ R/prepare_trend_graph.R
@@ -0,0 +1,41 @@
+#' @title Prepares a Trend Graph
+#'
+#' @description
+#' Reads a data frame and line plots all variables (which need to be numeric)
+#'  by an ordered factor.
+#'
+#' @param df Data frame containing the ordered factor and a set of numerical variables
+#' to be plotted
+#' @param xvar a string countaining the column name of the ordered factor
+#'
+#' @return A list containing two items:
+#' \itemize{
+#'  \item{"df"}{A data frame containg the plotted means and standard errors}
+#'  \item{"plot}{The plot as returned by ggplot}
+#' }
+#'
+#' @examples
+#' df <- data.frame(year = floor(time(EuStockMarkets)), EuStockMarkets)
+#' graph <- prepare_trend_graph(df, "year")
+#' graph$plot
+#' @export
+
+
+
+prepare_trend_graph <- function(df, xvar) {
+  gf <- tidyr::gather_(data = df,
+                       key_col = "variable",
+                       value_col = "value",
+                       gather_cols = colnames(df)[!(xvar == colnames(df))]) %>%
+    dplyr::group_by_at(dplyr::vars("variable", xvar)) %>%
+    dplyr::summarise(mean = mean(value, na.rm=TRUE),
+              se = stats::sd(value, na.rm=TRUE)/sqrt(length(which(!is.na(value)))))
+  gf <- as.data.frame(gf)
+  gf[,xvar] <- as.numeric(gsub("[^0-9\\.]", "", as.character(gf[,xvar])))
+
+  plot <- ggplot2::ggplot(gf, ggplot2::aes_string(x=xvar, colour="variable")) +
+    ggplot2::geom_errorbar(ggplot2::aes(ymin = mean-se, ymax = mean+se), width = .1) +
+    ggplot2::geom_line(ggplot2::aes(y = mean)) +
+    ggplot2::geom_point(ggplot2::aes(y = mean)) + ggplot2::xlab(xvar) + ggplot2::ylab("")
+  list(df = gf, plot = plot)
+}

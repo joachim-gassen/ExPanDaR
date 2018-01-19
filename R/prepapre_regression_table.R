1c22c68679a481aa4306cf4171f99fc33e1b235d
joachim-gassen <gassen@wiwi.hu-berlin.de>
2018-01-19 12:37:29 +0100

    fix levels in prepare_regression_table()

--- R/prepapre_regression_table.R
+++ R/prepapre_regression_table.R
@@ -85,7 +85,8 @@ prepare_regression_table <- function(df, dvs, idvs, feffects = rep("", length(dv
                           idvs = idvs,
                           feffects = feffects,
                           clusters = clusters)
-    mby <- lapply(unique(df[,byvar]), function(x) estimate_model(df[df[,byvar] == x,], datalist))
+    bylevels <- unique(df[,byvar])[order(unique(df[,byvar]))]
+    mby <- lapply(bylevels, function(x) estimate_model(df[df[,byvar] == x,], datalist))
     models <- list()
     models[[1]] <- estimate_model(df, datalist)
     for (i in 2:(length(mby) + 1))

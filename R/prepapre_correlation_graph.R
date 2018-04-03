5f50d2e5bee6d93ed0443a6ff473b2744289eb0c
joachim-gassen <gassen@wiwi.hu-berlin.de>
2018-04-03 16:33:49 +0200

    Fixed the consequenes of Hmisc:rcorr returning r/rho values > 1

--- R/prepapre_correlation_graph.R
+++ R/prepapre_correlation_graph.R
@@ -27,11 +27,15 @@ prepare_correlation_graph <- function(df) {
   scorr <- Hmisc::rcorr(as.matrix(df), type="spearman")
   correl_r <- pcorr$r
   correl_r[lower.tri(correl_r)] <- scorr$r[lower.tri(scorr$r)]
+  # Hmisc:rcorr returns r/rho values > 1 from time to time
+  # (of course only by a very small margin) and this
+  # causes corrplot to choke
+  correl_r <- pmin(correl_r,1)
   correl_n <- pcorr$n
   correl_n[lower.tri(correl_n)] <- scorr$n[lower.tri(scorr$n)]
   correl_p <- pcorr$P
   correl_p[lower.tri(correl_p)] <- scorr$P[lower.tri(scorr$P)]
-  corrplot::corrplot(pcorr$r, type = "upper", method="ellipse", tl.pos="lt", tl.col="black", bg="grey90", addgrid.col="white")
-  corrplot::corrplot(scorr$r, add=TRUE, type = "lower", diag=FALSE, tl.pos="n", cl.pos="n", method="ellipse", bg="grey90", addgrid.col="white")
+  corrplot::corrplot(correl_r, type = "upper", method="ellipse", tl.pos="lt", tl.col="black", bg="grey90", addgrid.col="white")
+  corrplot::corrplot(correl_r, add=TRUE, type = "lower", diag=FALSE, tl.pos="n", cl.pos="n", method="ellipse", bg="grey90", addgrid.col="white")
   return(list(df_corr = correl_r, df_prob = correl_p, df_n = correl_n))
 }
